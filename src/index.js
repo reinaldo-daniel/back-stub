d9274ad6d337f06c10feae1449e65d1287ab2cb4
Reinaldo_Daniel <reinaldo@smarppy.com>
2023-05-25 14:41:06 -0300

    correções

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express from "express";
-import bodyParser from "body-parser";
 import appConfig from "./config/appConfig.js";
 
 import presences from "./domains/presence/routes.js";
@@ -9,7 +8,7 @@ import schedules from "./domains/schedule/routes.js";
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/presence", presences);
 app.use("/schedule", schedules);
