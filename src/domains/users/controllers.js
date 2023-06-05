b69f48b05708cb864967b5fd2210014cb3340cca
Amandaallis <amanda@smarppy.com>
2023-06-05 13:23:26 -0300

    criado função para verificar se o e-mail já foi utilizado

--- src/domains/users/controllers.js
+++ src/domains/users/controllers.js
@@ -1,3 +1,5 @@
+import Users from "./model.js";
+
 async function users(request, response, next) {
     try {
         response.json({ teste: "acessou o controller de users" });
@@ -6,6 +8,27 @@ async function users(request, response, next) {
     }
 }
 
+function findUserByEmail(email) {
+    return Users.query().findOne({ email });
+}
+
+async function emailIsUsed(request, response) {
+    try {
+        const { email } = request.body;
+        const user = await findUserByEmail(email);
+        if (user) {
+            return response.status(404).json({
+                mensage: "Já existe um cadastro com esse e-mail. Por gentileza, tente novamente.",
+            });
+        }
+
+        return response.status(200).send();
+    } catch (err) {
+        return response.json(err);
+    }
+}
+
 export default {
     users,
+    emailIsUsed,
 };
