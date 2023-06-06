147250ce329994b9085cf48949be94ba7039b71f
Reinaldo_Daniel <reinaldo@smarppy.com>
2023-06-06 10:13:16 -0300

    criando valições

--- src/domains/users/validators.js
+++ src/domains/users/validators.js
@@ -1,7 +1,65 @@
 import Joi from "joi";
 
-const usersSchema = Joi.object();
+const TYPE = {
+    ADM: "ADM",
+    STUDENT: "STUDENT",
+    EMPLOYEE: "EMPLOYEE",
+};
+
+const loginUserSchema = Joi.object({
+    email: Joi.string()
+        .trim()
+        .email()
+        .lowercase(),
+
+    password: Joi.string()
+        .trim()
+        .min(6)
+        .max(200),
+});
+
+const userRegister = Joi.object({
+    name: Joi.string()
+        .trim()
+        .max(100)
+        .required(),
+
+    email: Joi.string()
+        .trim()
+        .email()
+        .required(),
+
+    password: Joi.string()
+        .trim()
+        .max(80)
+        .required(),
+
+    type: Joi.string()
+        .valid(TYPE)
+        .required(),
+});
+
+const userUpdateRegister = Joi.object({
+    name: Joi.string()
+        .trim()
+        .max(100),
+
+    email: Joi.string()
+        .trim()
+        .email(),
+
+    password: Joi.string()
+        .trim()
+        .max(80),
+
+    type: Joi.string()
+        .valid(TYPE),
+
+    situaction: Joi.boolean(),
+});
 
 export default {
-    usersSchema,
+    loginUserSchema,
+    userRegister,
+    userUpdateRegister,
 };
