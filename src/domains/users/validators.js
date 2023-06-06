b7d3b84ae57d57ef89bf8a1d1ee4b91b5ee7b912
Reinaldo_Daniel <reinaldo@smarppy.com>
2023-06-06 10:27:14 -0300

    correções

--- src/domains/users/validators.js
+++ src/domains/users/validators.js
@@ -39,6 +39,20 @@ const userRegister = Joi.object({
         .required(),
 });
 
+const userUpdatePerfil = Joi.object({
+    name: Joi.string()
+        .trim()
+        .max(100),
+
+    email: Joi.string()
+        .trim()
+        .email(),
+
+    password: Joi.string()
+        .trim()
+        .max(80),
+});
+
 const userUpdateRegister = Joi.object({
     name: Joi.string()
         .trim()
@@ -62,4 +76,5 @@ export default {
     loginUserSchema,
     userRegister,
     userUpdateRegister,
+    userUpdatePerfil,
 };
