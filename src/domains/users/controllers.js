ee1fab6aa36830421e7c4d7b499091df82454b64
guimendes <guilherme@smarppy.com>
2023-06-06 09:59:22 -0300

    retirada de comentários

--- src/domains/users/controllers.js
+++ src/domains/users/controllers.js
@@ -2,7 +2,7 @@ import Users from "./model.js";
 
 async function getUsers(req, res, next) {
     try {
-        const users = await Users.query().select("*"); // SELECT * FROM users;
+        const users = await Users.query().select("*");
         res.json(users);
     } catch (error) {
         next(error);
@@ -13,9 +13,9 @@ async function createUser(req, res, next) {
     try {
         const { name, email, password } = req.body;
 
-        const newUser = await Users.query().insert({ name, email, password }); // INSERT INTO users (name, email, password) VALUES (name, email, password);
+        const newUser = await Users.query().insert({ name, email, password });
 
-        res.status(201).json(newUser); // Retorna o usuário criado
+        res.status(201).json(newUser);
     } catch (error) {
         next(error);
     }
