93bffa27bb04b8fd3fa6a5c728a0054b883f5b83
Reinaldo_Daniel <reinaldo@smarppy.com>
2023-06-21 14:33:33 -0300

    correção

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import Knex from "knex";
-import morgan from "morgan";
 import { Model } from "objection";
 
 import knexConfig from "../database/knexfile";
@@ -12,6 +11,7 @@ import rooms from "./domains/rooms/routes";
 import schedules from "./domains/scheduleControl/routes";
 import users from "./domains/users/routes";
 import authMiddleware from "./middleware/authMiddleware";
+import reqLogMiddleware from "./middleware/reqLogMiddleware";
 
 Model.knex(Knex(knexConfig));
 
@@ -19,8 +19,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
+app.use(reqLogMiddleware);
 app.use(authMiddleware);
 
 app.use("/presence", presences);
