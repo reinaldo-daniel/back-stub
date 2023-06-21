571bd0ec576fd90400f562dd921fe36f9b568f38
Reinaldo_Daniel <reinaldo@smarppy.com>
2023-06-21 11:44:52 -0300

    adasda

--- src/domains/users/routes.js
+++ src/domains/users/routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-import controllers from "./controllers.js";
+import { login } from "./controllers";
 
 const router = Router();
 
-router.post("/login", controllers.login);
+router.post("/login", login);
 
 export default router;
